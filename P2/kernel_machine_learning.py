f8082b9ad2f02cbf005047b311b24f9591be9596
Ludvins <theludvins@gmail.com>
2021-02-27 17:12:14 +0100

    PCA

--- P2/kernel_machine_learning.py
+++ P2/kernel_machine_learning.py
@@ -63,6 +63,17 @@ def rbf_kernel(
     return A * np.exp(-0.5 * (d / ls)**2)
 
 
+def compute_hat(
+    K: np.ndarray, 
+    ) -> np.ndarray :
+    """
+    Auxiliary function to kernel_pca. Computes Gram matrix of the centered kernel given
+    the Gram matrix of the kernel.
+    """
+    N = K.shape[0]
+    ones = np.ones(K.shape)
+    return K - 1/N*(K@ones) - 1/N*(ones@K) + 1/(N**2)*(ones@K@ones)
+
 def kernel_pca(
     X: np.ndarray,
     X_test: np.ndarray,
@@ -97,6 +108,30 @@ def kernel_pca(
 
     """
 
-    # NOTE <YOUR CODE HERE>.
+    # Build Gram Matrix
+    K = kernel(X, X)
+
+    # Gram matrix of centered kernel
+    K_hat = compute_hat(K)
+
+    # Compute eigen vectors and values 
+    lambda_eigenvals, alpha_eigenvecs = np.linalg.eig(K_hat)
+
+    # Handle complex values
+    if np.iscomplexobj(lambda_eigenvals):
+        threshold_imaginary_part = 1.0e-6
+        max_imaginary_part = np.max(
+            np.abs(np.imag(lambda_eigenvals))
+        )
+        if max_imaginary_part > threshold_imaginary_part:
+            warnings.warn(
+                'Maximum imaginary part is {}'.format(max_imaginary_part)
+            )
+ 
+        lambda_eigenvals = np.real(lambda_eigenvals)
+
+    K_test = kernel(X_test, X_test)
+    K_hat_test = compute_hat(K_test)
+    X_test_hat = K_hat_test@alpha_eigenvecs
 
     return X_test_hat, lambda_eigenvals, alpha_eigenvecs
